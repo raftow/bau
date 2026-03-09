432593494502f08ee2b4492c280b327aa0235de3
raftow <rboubaker@tvtc.gov.sa>
2026-03-09 16:47:59 +0300

    work on workflow project v1

--- main.php
+++ main.php
@@ -1,14 +1,3 @@
 <?php
-
 $file_dir_name = dirname(__FILE__); 
-include_once ("$file_dir_name/ini.php");
-include_once ("$file_dir_name/module_config.php");
-
-
-
-//echo "The system is under maintenance please contact administrator or try later.";
-
-
-AfwMainPage::echoMainPage($MODULE);
-
-?>
+include("$file_dir_name/../ums/standard_main.php"); 
