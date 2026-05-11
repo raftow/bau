20fcbf330f0a304a515d564ccc8ad13c137a7515
rafiq boubaker <rafik.towati@gmail.com>
2026-05-11 22:51:16 +0300

    some important improvement

--- migrations/migration_00001.php
+++ migrations/migration_00001.php
@@ -6,6 +6,5 @@ try {
 
     AfwDatabase::db_query("ALTER TABLE " . $server_db_prefix . "bau.goal_concern add   arole_id int(11) NOT NULL DEFAULT 0  AFTER operation_men");
 } catch (Exception $e) {
-    $migration_error .= " " . $e->getMessage();
+    $migration_error = " " . $e->getMessage();
 }
-
